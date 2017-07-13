0ee5345f01f26128eebfc97e28720cb0911d4125
itsquall <sergioacaraballo@gmail.com>
2017-07-13 14:55:52 +0200

    Add public function to create attributes

--- lib/attribrutex.ex
+++ lib/attribrutex.ex
@@ -1,18 +1,58 @@
 defmodule Attribrutex do
   @moduledoc """
-  Documentation for Attribrutex.
+  Public functions to manage custom fields
   """
+  alias Attribrutex.CustomField
+
+  @repo Attribrutex.RepoClient.repo
 
   @doc """
-  Hello world.
+  Creates a new field for a module.
 
-  ## Examples
+  If you need specify a context, in example, the custom field belongs
+  to a specific user, you can use the opts to set a `context_id` and a
+  `context_type`.
 
-      iex> Attribrutex.hello
-      :world
+  ## Example
+
+     Attribrutex.create_custom_field("location", :string, User)
+
+  Now, the custom field belongs to the `User` model
+
+  ## Context example
+
+     Attribrutex.create_custom_field("location", :string, User, context_id: user.id, context_type: "User")
+
+  Setting a context, you can make fields available only for an specific resource
 
   """
-  def hello do
-    :world
+  def create_custom_field(key, type, module, opts \\ [])
+  def create_custom_field(key, type, module, []) do
+    attrs = %{
+      key: key,
+      field_type: type,
+      fieldable_type: module_name(module)
+    }
+
+    insert_custom_field(attrs)
   end
+  def create_custom_field(key, type, module, [context_id: context_id, context_type: context_type]) do
+    attrs = %{
+      key: key,
+      field_type: type,
+      fieldable_type: module_name(module),
+      context_id: context_id,
+      context_type: context_type
+    }
+
+    insert_custom_field(attrs)
+  end
+
+  defp insert_custom_field(attrs) do
+    with changeset <- CustomField.changeset(%CustomField{}, attrs) do
+      @repo.insert(changeset)
+    end
+  end
+
+  defp module_name(module), do: module |> Module.split |> List.last
 end
--- test/attribrutex_test.exs
+++ test/attribrutex_test.exs
@@ -1,2 +1,34 @@
 defmodule AttribrutexTest do
+  use ExUnit.Case
+
+  doctest Attribrutex
+
+  @repo Attribrutex.RepoClient.repo
+
+  setup do
+    @repo.delete_all(Attribrutex.CustomField)
+    :ok
+  end
+
+  test "create_custom_field/4" do
+    {status, resource} =
+      Attribrutex.create_custom_field("sample", :string, AttribrutexUser)
+
+    assert status == :ok
+    assert resource.key == "sample"
+    assert resource.field_type == :string
+    assert resource.fieldable_type == "AttribrutexUser"
+  end
+
+  test "create_custom_field/4 with context" do
+    {status, resource} =
+      Attribrutex.create_custom_field("sample", :string, AttribrutexUser, context_id: 1, context_type: "User")
+
+    assert status == :ok
+    assert resource.key == "sample"
+    assert resource.field_type == :string
+    assert resource.fieldable_type == "AttribrutexUser"
+    assert resource.context_id == 1
+    assert resource.context_type == "User"
+  end
 end
