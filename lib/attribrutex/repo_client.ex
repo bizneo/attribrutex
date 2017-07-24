69dad35d032f7d969a83c08cd4b746f2e6f2d499
itsquall <sergioacaraballo@gmail.com>
2017-07-24 14:29:28 +0200

    Rename default Repo

--- lib/attribrutex/repo_client.ex
+++ lib/attribrutex/repo_client.ex
@@ -6,5 +6,5 @@ defmodule Attribrutex.RepoClient do
   @doc """
   Gets the configured repo module or defaults to Repo if none configured
   """
-  def repo, do: Application.get_env(:attribrutex, :repo, Repo)
+  def repo, do: Application.get_env(:attribrutex, :repo, Attribrutex.Repo)
 end
